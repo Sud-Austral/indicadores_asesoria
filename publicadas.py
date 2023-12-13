d3e53d8f689924699c334cd31e6eeee3ca6deae8
lmonsalve22 <lmonsalve22@gmail.com>
2023-12-13 10:53:54 -0300

    comentarios

--- publicadas.py
+++ publicadas.py
@@ -1,14 +1,15 @@
 import pandas as pd
 
 def proceso2():
-    print("Comenzamos publicadas...")
+    #Se lee el archivo Tabla_buscador_indicadores.xlsx
     df = pd.read_excel("Tabla_buscador_indicadores.xlsx")
-    #print("Comenzamos publicadas... 1")
+    #Se definen las columnas que se mantendran
     base = ['Indicador', 'Capítulo', 'Categoría GEO (UNEP)', 'Categoría EAG (OECD)','Enlace visualización IFG','ODS', 'Fuente', 'Nombre indicador separado']
+    #Se mantienen las columnas definidas en base y se crean 2 columnas nuevas, Región y Valor
     df_unpivoted = pd.melt(df, id_vars=base, var_name='Región', value_name='Valor')
-    #print("Comenzamos publicadas...2")
+    #Solo se conservan los registros con Valor SI
     df_unpivoted = df_unpivoted[df_unpivoted["Valor"] == "SI"]
-    #print("Comenzamos publicadas...3 ")
+    #Se guarda los registros para ser utilizadops más tarde
     with open('registro.json', 'w', encoding='utf-8') as file:
         df_unpivoted.to_json(file, orient ='records',force_ascii=False,indent=1)
     
